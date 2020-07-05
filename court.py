978833e898e488610bc8ebd139a346b4979e91a9
Devin Powers <50432490+devinpowers@users.noreply.github.com>
2020-07-05 13:02:55 -0400

    Update court.py

--- court.py
+++ court.py
@@ -1,15 +1,13 @@
-
 import matplotlib.pylab as plt
 import matplotlib as mpl
 from matplotlib.patches import Circle, Rectangle, Arc
 
-from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
 
 #start plotting
 
 #lw = line width = standard for court
 
-def create_ncaa_half_court(ax=None, court_color='white', lw=3, lines_color='black', lines_alpha=0.5, paint_fill='Green', paint_alpha=0.4, inner_arc= True):
+def create_ncaa_half_court(ax=None, court_color='white', lw=3, lines_color='black', lines_alpha=0.5, paint_fill='Green', paint_alpha=0.4):
     
     # combining two independent plots with matplotlib
     # ax = axes
@@ -19,7 +17,7 @@ def create_ncaa_half_court(ax=None, court_color='white', lw=3, lines_color='blac
         
     # Center Circle
     # Circle(xy, radius, color, fill)
-    center_circle = Circle((50/2, 94/2), 6, inewidth=lw, color=lines_color, lw=lw, fill=False, alpha=lines_alpha)
+    center_circle = Circle((50/2, 94/2), 6, linewidth=lw, color=lines_color, lw=lw, fill=False, alpha=lines_alpha)
     
     #Basketball Hoop
     hoop = Circle((50/2, 5.25), 1.5 / 2, linewidth=lw, color=lines_color, lw=lw, fill=False, alpha=lines_alpha)
@@ -32,6 +30,14 @@ def create_ncaa_half_court(ax=None, court_color='white', lw=3, lines_color='blac
     paint_boarder = Rectangle(((50/2)-6, 0), 12, 18.833333, fill=False, alpha=lines_alpha, lw=lw, edgecolor=lines_color)
     
     arc = Arc((50/2, 18.833333), 12, 12, theta1=-0, theta2=180, color=lines_color, lw=lw, alpha=lines_alpha)
+    
+    
+     # Add Patches
+    ax.add_patch(paint)
+    ax.add_patch(paint_boarder)
+    ax.add_patch(center_circle)
+    ax.add_patch(hoop)
+    ax.add_patch(arc)
     
     block1 = Rectangle(((50/2)-6-0.666, 7), 0.666, 1, fill=True, alpha=lines_alpha,lw=0, edgecolor=lines_color,facecolor=lines_color)
     
@@ -64,5 +70,44 @@ def create_ncaa_half_court(ax=None, court_color='white', lw=3, lines_color='blac
     
     
     # 3 Point Line !!!
+    
+    three_pt = Arc((50/2, 6.25), 44.291, 44.291, theta1=12,theta2=168, color=lines_color, lw=lw,alpha=lines_alpha)
+    
+    ax.plot((3.34, 3.34), (0, 11.20),color=lines_color, lw=lw, alpha=lines_alpha)
+    
+    ax.plot((50-3.34, 50-3.34), (0, 11.20),color=lines_color, lw=lw, alpha=lines_alpha)
+    
+    ax.add_patch(three_pt)
+    
+    inner_arc = Arc((50/2, 18.833333), 12, 12, theta1=180, theta2=0, color=lines_color, lw=lw,alpha=lines_alpha, ls='--')
+    
+    ax.add_patch(inner_arc)
+    
+    # Restricted Area Marker
+    restricted_area = Arc((50/2, 6.25), 8, 8, theta1=0, theta2=180, color=lines_color, lw=lw,alpha=lines_alpha)
+    ax.add_patch(restricted_area)
+    
+    # Backboard
+    ax.plot(((50/2) - 3, (50/2) + 3), (4, 4), color=lines_color, lw=lw*1.5, alpha=lines_alpha)
+    
+    ax.plot( (50/2, 50/2), (4.3, 4), color=lines_color,lw=lw, alpha=lines_alpha)
+
+    # Half Court Line
+    ax.axhline(94/2, color=lines_color, lw=lw, alpha=lines_alpha)
+    
+    ax.set_xlim(0, 50)
+    ax.set_ylim(0, 94/2 + 2)
+    ax.set_facecolor(court_color)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xlabel('')
+    return ax
+
+fig, ax = plt.subplots(figsize=(11, 11.2))
+
+create_ncaa_half_court(ax,paint_alpha=0.4)
+
+plt.show()
+
     
     
